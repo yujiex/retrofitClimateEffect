abb0451510d50af58293c7c772e5b62273ef7380
yujiex <shenqing1225@gmail.com>
2020-05-26 17:04:02 -0400

    Split data preparation to a different file
    - split the non-action related input data compilation to a different file
    - add estimation with 4 different action classification approaches

---
+++ data-raw/prepare_cf_data.R
@@ -0,0 +1,46 @@
+library("dplyr")
+
+load("../data/retrofit.alldata.rda")
+
+pre.nonenergy = retrofit.alldata %>%
+  dplyr::filter(retro.status == "pre") %>%
+  dplyr::mutate(is.office = (`Building_Type`=="Office")) %>%
+  dplyr::select(-ownership, -variable, -retro.status, -mean.kbtu, -name, -count,
+                -start, -end, -`Building_Type`, -HDD, -CDD) %>%
+  dplyr::distinct() %>%
+  dplyr::mutate(REGNNUM = paste0("Region ", REGNNUM)) %>%
+  dplyr::mutate(value=T) %>%
+  tidyr::spread(REGNNUM, value, fill=F) %>%
+  dplyr::mutate(BLDGCAT= paste0("Category ", BLDGCAT)) %>%
+  dplyr::mutate(value=T) %>%
+  tidyr::spread(BLDGCAT, value, fill=F) %>%
+  {.}
+
+pre.energy = retrofit.alldata %>%
+  dplyr::filter(!(variable %in% c("KWDMD", "OIL"))) %>%
+  dplyr::filter(retro.status == "pre") %>%
+  dplyr::select(BLDGNUM, is.real.retrofit,
+                `Substantial_Completion_Date`, variable, mean.kbtu, GROSSSQFT) %>%
+  dplyr::mutate(kbtu.per.sqft = mean.kbtu/GROSSSQFT) %>%
+  dplyr::select(-mean.kbtu, -GROSSSQFT) %>%
+  tidyr::spread(variable, kbtu.per.sqft, fill=0) %>%
+  {.}
+
+eui.diff.total = retrofit.alldata %>%
+  dplyr::filter(!(variable %in% c("KWDMD", "OIL"))) %>%
+  dplyr::select(BLDGNUM, Substantial_Completion_Date, retro.status,
+                is.real.retrofit, variable, mean.kbtu, GROSSSQFT) %>%
+  dplyr::mutate(kbtu.per.sqft = mean.kbtu / GROSSSQFT) %>%
+  dplyr::select(-mean.kbtu, -GROSSSQFT) %>%
+  tidyr::spread(retro.status, kbtu.per.sqft) %>%
+  dplyr::mutate(eui.diff = post - pre) %>%
+  dplyr::select(-post, -pre) %>%
+  {.}
+
+non.action.data = pre.nonenergy %>%
+  dplyr::left_join(pre.energy, by=c("BLDGNUM", "is.real.retrofit", "Substantial_Completion_Date")) %>%
+  dplyr::left_join(eui.diff.total, by=c("BLDGNUM", "is.real.retrofit", "Substantial_Completion_Date")) %>%
+  dplyr::filter(variable %in% c("KWHR", "GAS")) %>%
+  {.}
+
+usethis::use_data(non.action.data)
--- data-raw/test_model.R
+++ data-raw/test_model.R
@@ -1,141 +1,99 @@
 library("dplyr")
 library("grf")
 
-load("../data/retrofit.alldata.rda")
-load("../data/retrofit.alldata.highprop.rda")
+## load("../data/retrofit.alldata.rda")
+## load("../data/retrofit.alldata.highprop.rda")
 
-load("../data/retrofit_prev_actions.rda")
+load("../data/retrofit_prev_actions_toplevel.rda")
+load("../data/retrofit_prev_actions_highlevel.rda")
+load("../data/retrofit_prev_actions_joint_highlevel.rda")
+load("../data/retrofit_prev_actions_detaillevel.rda")
 
-dfs.by.action = retrofit_prev_actions %>%
-  dplyr::group_by(target.action) %>%
-  dplyr::group_split() %>%
-  {.}
-
-pre.nonenergy = retrofit.alldata.highprop %>%
-  dplyr::filter(retro.status == "pre") %>%
-  dplyr::mutate(is.office = (`Building_Type`=="Office")) %>%
-  dplyr::select(-ownership, -variable, -retro.status, -mean.kbtu, -name, -count,
-                -start, -end, -`Building_Type`, -`propensity.estimate`, -HDD, -CDD) %>%
-  dplyr::distinct() %>%
-  dplyr::mutate(REGNNUM = paste0("Region ", REGNNUM)) %>%
-  dplyr::mutate(value=T) %>%
-  tidyr::spread(REGNNUM, value, fill=F) %>%
-  dplyr::mutate(BLDGCAT= paste0("Category ", BLDGCAT)) %>%
-  dplyr::mutate(value=T) %>%
-  tidyr::spread(BLDGCAT, value, fill=F) %>%
-  {.}
-
-pre.energy = retrofit.alldata.highprop %>%
-  dplyr::filter(!(variable %in% c("KWDMD", "OIL"))) %>%
-  dplyr::filter(retro.status == "pre") %>%
-  dplyr::select(BLDGNUM, is.real.retrofit,
-                `Substantial_Completion_Date`, variable, mean.kbtu, GROSSSQFT) %>%
-  dplyr::mutate(kbtu.per.sqft = mean.kbtu/GROSSSQFT) %>%
-  dplyr::select(-mean.kbtu, -GROSSSQFT) %>%
-  tidyr::spread(variable, kbtu.per.sqft, fill=0) %>%
-  {.}
-
-eui.diff.total = retrofit.alldata.highprop %>%
-  dplyr::filter(!(variable %in% c("KWDMD", "OIL"))) %>%
-  dplyr::select(BLDGNUM, Substantial_Completion_Date, retro.status,
-                is.real.retrofit, variable, mean.kbtu, GROSSSQFT) %>%
-  dplyr::mutate(kbtu.per.sqft = mean.kbtu / GROSSSQFT) %>%
-  dplyr::select(-mean.kbtu, -GROSSSQFT) %>%
-  ## dplyr::group_by(BLDGNUM, Substantial_Completion_Date, retro.status,
-  ##                 is.real.retrofit) %>%
-  ## dplyr::summarise(kbtu.per.sqft = sum(kbtu.per.sqft)) %>%
-  tidyr::spread(retro.status, kbtu.per.sqft) %>%
-  dplyr::mutate(eui.diff = post - pre) %>%
-  dplyr::select(-post, -pre) %>%
-  {.}
-
-dfs.by.fuel = pre.nonenergy %>%
-  dplyr::left_join(pre.energy, by=c("BLDGNUM", "is.real.retrofit", "Substantial_Completion_Date")) %>%
-  dplyr::left_join(eui.diff.total, by=c("BLDGNUM", "is.real.retrofit", "Substantial_Completion_Date")) %>%
-  dplyr::filter(variable %in% c("KWHR", "GAS")) %>%
-  dplyr::group_by(variable) %>%
-  dplyr::group_split() %>%
-  {.}
-
-## dfs.by.fuel = retrofit.alldata.highprop %>%
-##   ## dplyr::filter(!(variable %in% c("KWDMD", "OIL"))) %>%
-##   dplyr::filter(variable %in% c("KWHR", "GAS")) %>%
-##   dplyr::group_by(variable) %>%
-##   dplyr::group_split() %>%
-##   {.}
-
-set.seed(0)
+load("../data/non.action.data.rda")
 
 options(tibble.width=NULL)
 
-result = lapply(dfs.by.action, function(df.action) {
-  result.inner = lapply(dfs.by.fuel, function(df.fuel) {
-    action = df.action$target.action[[1]]
-    fuel.type = df.fuel$variable[[1]]
-    print(sprintf("action: %s, fuel: %s", action, fuel.type))
-    df.allfeature = df.fuel %>%
-      dplyr::left_join(df.action, by=c("BLDGNUM"="Building_Number", "Substantial_Completion_Date"="target.date")) %>%
-      dplyr::filter(!(is.real.retrofit & is.na(target.action))) %>%
-      dplyr::select(-target.action) %>%
-      dplyr::mutate_at(vars(`Advanced Metering`:`Lighting`),
-                      function (x) {ifelse(is.na(x), 0, x)}) %>%
-      dplyr::mutate_if(is.logical, as.numeric) %>%
-      dplyr::select(-variable) %>%
-      {.}
-    df.allfeature %>%
-      distinct(BLDGNUM, is.real.retrofit) %>%
-      dplyr::group_by(is.real.retrofit) %>%
-      dplyr::count() %>%
-      dplyr::ungroup()
-    X = df.allfeature %>%
-      select(-BLDGNUM, -is.real.retrofit, -`Substantial_Completion_Date`, -eui.diff) %>%
-      as.matrix() %>%
-      {.}
-    Y = df.allfeature$eui.diff
-    W = df.allfeature$is.real.retrofit
-    c.forest <- grf::causal_forest(X, Y, W)
-    c.pred <- predict(c.forest, estimate.variance = T)
-    ## plot importance
-    importance = variable_importance(c.forest)
-    df.var.importance = tibble::tibble(variable = colnames(X),
-                                      importance = as.vector(importance)) %>%
-      dplyr::mutate(action = action, fuel=fuel.type) %>%
-      {.}
-    df.result = df.allfeature %>%
-      dplyr::bind_cols(c.pred) %>%
-      dplyr::mutate(action = action, fuel=fuel.type) %>%
-      {.}
-    return(list("df.result"=df.result, "df.var.importance"=df.var.importance, "model"=c.forest))
+## prev.action: df with previous action
+## fuel.and.other: df with other fields
+## kw: key words in file naming
+fit.cf <- function(prev.action, fuel.and.other, kw) {
+  dfs.by.action = prev.action %>%
+    dplyr::group_by(target.action) %>%
+    dplyr::group_split() %>%
+    {.}
+  dfs.by.fuel = fuel.and.other %>%
+    dplyr::group_by(variable) %>%
+    dplyr::group_split() %>%
+    {.}
+  result = lapply(dfs.by.action, function(df.action) {
+    result.inner = lapply(dfs.by.fuel, function(df.fuel) {
+      action = df.action$target.action[[1]]
+      name.all.actions = names(df.action)
+      name.all.actions <- name.all.actions[!(name.all.actions %in% c("Building_Number", "target.action", "target.date"))]
+      fuel.type = df.fuel$variable[[1]]
+      print(sprintf("action: %s, fuel: %s, %s", action, fuel.type, kw))
+      df.allfeature = df.fuel %>%
+        dplyr::left_join(df.action, by=c("BLDGNUM"="Building_Number", "Substantial_Completion_Date"="target.date")) %>%
+        dplyr::filter(!(is.real.retrofit & is.na(target.action))) %>%
+        dplyr::select(-target.action) %>%
+        dplyr::mutate_at(name.all.actions,
+                         function (x) {ifelse(is.na(x), 0, x)}) %>%
+        dplyr::mutate_if(is.logical, as.numeric) %>%
+        dplyr::select(-variable) %>%
+        {.}
+      df.allfeature %>%
+        distinct(BLDGNUM, is.real.retrofit) %>%
+        dplyr::group_by(is.real.retrofit) %>%
+        dplyr::count() %>%
+        dplyr::ungroup()
+      X = df.allfeature %>%
+        select(-BLDGNUM, -is.real.retrofit, -`Substantial_Completion_Date`, -eui.diff) %>%
+        as.matrix() %>%
+        {.}
+      Y = df.allfeature$eui.diff
+      W = df.allfeature$is.real.retrofit
+      c.forest <- grf::causal_forest(X, Y, W)
+      c.pred <- predict(c.forest, estimate.variance = T)
+      ## plot importance
+      importance = variable_importance(c.forest)
+      df.var.importance = tibble::tibble(variable = colnames(X),
+                                        importance = as.vector(importance)) %>%
+        dplyr::mutate(action = action, fuel=fuel.type) %>%
+        {.}
+      df.result = df.allfeature %>%
+        dplyr::bind_cols(c.pred) %>%
+        dplyr::mutate(action = action, fuel=fuel.type) %>%
+        {.}
+      return(list("df.result"=df.result, "df.var.importance"=df.var.importance, "model"=c.forest))
+    })
   })
-})
+  unnested = unlist(result, recursive = F)
+  dfs.result.prediction = lapply(unnested, function(li) {
+    li$df.result
+  })
+  dfs.var.importance = lapply(unnested, function(li) {
+    li$df.var.importance
+  })
+  dfs.result.prediction %>%
+    dplyr::bind_rows() %>%
+    readr::write_csv(sprintf("../tables/grf_result_%s.csv", kw))
+  dfs.var.importance %>%
+    dplyr::bind_rows() %>%
+    readr::write_csv(sprintf("../tables/grf_var_importance_%s.csv", kw))
+}
 
-unnested = unlist(result, recursive = F)
+## output retrofit effect estimates
+set.seed(0)
+fit.cf(retrofit_prev_actions_highlevel, non.action.data, "highlevel")
 
-dfs.result.prediction = lapply(unnested, function(li) {
-  li$df.result
-})
+set.seed(0)
+fit.cf(retrofit_prev_actions_toplevel, non.action.data, "toplevel")
 
-dfs.var.importance = lapply(unnested, function(li) {
-  li$df.var.importance
-})
+set.seed(0)
+fit.cf(retrofit_prev_actions_joint_highlevel, non.action.data, "joint_highlevel")
 
-## the list of features used
-dfs.result.prediction %>%
-  dplyr::bind_rows() %>%
-  dplyr::select(-starts_with("Region ")) %>%
-  dplyr::select(-starts_with("Category ")) %>%
-  dplyr::select(-eui.diff, -(predictions:fuel),
-                -(BLDGNUM:is.real.retrofit)) %>%
-  names() %>%
-  {.}
-
-dfs.result.prediction %>%
-  dplyr::bind_rows() %>%
-  readr::write_csv("../tables/grf_result.csv")
-
-dfs.var.importance %>%
-  dplyr::bind_rows() %>%
-  readr::write_csv("../tables/grf_var_importance.csv")
+set.seed(0)
+fit.cf(retrofit_prev_actions_detaillevel, non.action.data, "detaillevel")
 
 ## compare results under different climate scenario
 load("../data/cmip5.bin.period.rda")
