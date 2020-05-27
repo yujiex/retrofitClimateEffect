accb2417153d0009efa1758bf7583245300479ca
yujiex <shenqing1225@gmail.com>
2020-05-27 04:34:30 -0400

    Bundled basic cf fitting into a function

--- data-raw/test_model.R
+++ data-raw/test_model.R
@@ -13,6 +13,16 @@ load("../data/non.action.data.rda")
 
 options(tibble.width=NULL)
 
+## compare results under different climate scenario
+load("../data/cmip5.bin.period.rda")
+
+dfs.cmip5 = cmip5.bin.period %>%
+  dplyr::filter(period == "2050Jan through 2059Jan",
+                scenario == "rcp45") %>%
+  dplyr::group_by(scenario, period, model) %>%
+  dplyr::group_split() %>%
+  {.}
+
 ## prev.action: df with previous action
 ## fuel.and.other: df with other fields
 ## kw: key words in file naming
@@ -41,11 +51,6 @@ fit.cf <- function(prev.action, fuel.and.other, kw) {
         dplyr::mutate_if(is.logical, as.numeric) %>%
         dplyr::select(-variable) %>%
         {.}
-      df.allfeature %>%
-        distinct(BLDGNUM, is.real.retrofit) %>%
-        dplyr::group_by(is.real.retrofit) %>%
-        dplyr::count() %>%
-        dplyr::ungroup()
       X = df.allfeature %>%
         select(-BLDGNUM, -is.real.retrofit, -`Substantial_Completion_Date`, -eui.diff) %>%
         as.matrix() %>%
@@ -74,12 +79,55 @@ fit.cf <- function(prev.action, fuel.and.other, kw) {
   dfs.var.importance = lapply(unnested, function(li) {
     li$df.var.importance
   })
+  ## prediction for current climate
   dfs.result.prediction %>%
     dplyr::bind_rows() %>%
     readr::write_csv(sprintf("../tables/grf_result_%s.csv", kw))
   dfs.var.importance %>%
     dplyr::bind_rows() %>%
     readr::write_csv(sprintf("../tables/grf_var_importance_%s.csv", kw))
+  model.weights = readr::read_csv("climate_model_weights.csv") %>%
+    dplyr::select(-`Skill Weight`, -`Uniqueness Weight`) %>%
+    na.omit()
+  result.outer = lapply(unnested, function(li){
+    learned.model = li$model
+    old.data = li$df.result
+    fuel = old.data$fuel[[1]]
+    action = old.data$action[[1]]
+    result.inner = lapply(seq_along(dfs.cmip5), function(j) {
+      df.climate = dfs.cmip5[[j]]
+      period = df.climate$period[[1]]
+      scenario = df.climate$scenario[[1]]
+      modelname = df.climate$model[[1]]
+      print(paste(fuel, action, period, scenario, j, "----", modelname))
+      df = df.climate %>%
+        dplyr::select(-Latitude, -Longitude, -period, -model, -scenario) %>%
+        {.}
+      new.data <- old.data %>%
+        dplyr::select(-(`<10`:`>90`)) %>%
+        dplyr::inner_join(df, by="BLDGNUM") %>%
+        {.}
+      X.new = new.data %>%
+        dplyr::select(-BLDGNUM, -is.real.retrofit, -`Substantial_Completion_Date`, -eui.diff, -predictions, -debiased.error, -excess.error, -action, -fuel, -variance.estimates) %>%
+        as.matrix() %>%
+        {.}
+      prediction.new = predict(learned.model, X.new, estimate.variance = T)
+      df.inner = old.data %>%
+        dplyr::select(BLDGNUM, `Substantial_Completion_Date`, is.real.retrofit,
+                      action, fuel) %>%
+        dplyr::bind_cols(prediction.new) %>%
+        dplyr::rename(cmip5.predictions = predictions) %>%
+        dplyr::mutate(period=period, scenario=scenario, model=modelname) %>%
+        {.}
+      return(df.inner)
+    })
+    df.inner.agg = dplyr::bind_rows(result.inner)
+    return(df.inner.agg)
+  })
+  result.outer %>%
+    dplyr::bind_rows() %>%
+    dplyr::left_join(model.weights, by="model") %>%
+    readr::write_csv(sprintf("prediction_under_climate_change_%s.csv", kw))
 }
 
 ## output retrofit effect estimates
@@ -94,66 +142,3 @@ fit.cf(retrofit_prev_actions_joint_highlevel, non.action.data, "joint_highlevel"
 
 set.seed(0)
 fit.cf(retrofit_prev_actions_detaillevel, non.action.data, "detaillevel")
-
-## compare results under different climate scenario
-load("../data/cmip5.bin.period.rda")
-
-dfs.cmip5 = cmip5.bin.period %>%
-  dplyr::filter(period == "2050Jan through 2059Jan",
-                scenario == "rcp45") %>%
-  dplyr::group_by(scenario, period, model) %>%
-  dplyr::group_split() %>%
-  {.}
-
-set.seed(0)
-
-result.outer = lapply(unnested, function(li){
-  learned.model = li$model
-  old.data = li$df.result
-  fuel = old.data$fuel[[1]]
-  action = old.data$action[[1]]
-  result.inner = lapply(dfs.cmip5, function(df.climate) {
-    period = df.climate$period[[1]]
-    scenario = df.climate$scenario[[1]]
-    modelname = df.climate$model[[1]]
-    print(paste(fuel, action, period, scenario, modelname))
-    df = df.climate %>%
-      dplyr::select(-Latitude, -Longitude, -period, -model, -scenario) %>%
-      {.}
-    print(head(df))
-    new.data <- old.data %>%
-      dplyr::select(-(`<10`:`>90`)) %>%
-      dplyr::inner_join(df, by="BLDGNUM") %>%
-      {.}
-    X.new = new.data %>%
-      dplyr::select(-BLDGNUM, -is.real.retrofit, -`Substantial_Completion_Date`, -eui.diff, -predictions, -debiased.error, -excess.error, -action, -fuel) %>%
-      ## reorder data columns
-      ## dplyr::select(GROSSSQFT:htdd, `<10`:`>90`, is.office:Lighting) %>%
-      as.matrix() %>%
-      {.}
-    prediction.new = predict(learned.model, X.new, estimate.variance = T)
-    print(head(prediction.new$predictions))
-    df.inner = old.data %>%
-      dplyr::select(BLDGNUM, `Substantial_Completion_Date`, is.real.retrofit,
-                    action, fuel) %>%
-      dplyr::bind_cols(prediction.new) %>%
-      dplyr::rename(cmip5.predictions = predictions) %>%
-      dplyr::mutate(period=period, scenario=scenario, model=modelname) %>%
-      {.}
-    return(df.inner)
-  })
-  df.inner.agg = dplyr::bind_rows(result.inner)
-  return(df.inner.agg)
-})
-
-model.weights = readr::read_csv("climate_model_weights.csv") %>%
-  dplyr::select(-`Skill Weight`, -`Uniqueness Weight`) %>%
-  na.omit()
-
-result.outer %>%
-  dplyr::bind_rows() %>%
-  dplyr::left_join(model.weights, by="model") %>%
-  readr::write_csv("prediction_under_climate_change.csv")
-
-plot(grf::get_tree(unnested[[6]]$model, 5))
-
