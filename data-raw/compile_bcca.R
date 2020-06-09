eeb3b8e1140db4180642d236d81c7032bfb071e8
yujiex <shenqing1225@gmail.com>
2020-06-09 15:37:09 -0400

    Change parent folder setting to aggregate new climate data
    - compile data in bcca_now
    - return a three-year period aggregation and the overall aggregation

--- data-raw/compile_bcca.R
+++ data-raw/compile_bcca.R
@@ -1,66 +1,86 @@
+library("dplyr")
+
 ## This file compiles the tasavg into different climate change scenarios
 
-folders = list.files("cmip5/bcca/", "bcca5*")
+compiled.data = compiled.data = lapply (c("cmip5/bcca_now/", "cmip5/bcca/"), function(parent.folder) {
+  folders = list.files(parent.folder, "bcca5*")
+  result = lapply(folders, function(f) {
+    lines = readLines(paste0(parent.folder, f, "/MetaData.txt"))
+    period.line = lines[[5]]
+    period = gsub("Period:                ", "", period.line)
+    model.lines <- readLines(paste0(parent.folder, f, "/Projections5.txt"))
+    model = sapply(model.lines, function (m) {substr(m, 1, nchar(m) - 6)})
+    scenario = sapply(model.lines, function (m) {substr(m, nchar(m) - 4, nchar(m))})
+    return(tibble::tibble("model"=model, "period"=period, "scenario"=scenario, "folder"=f))
+  })
+  ## download summary
+  df.cmip5 = dplyr::bind_rows(result) %>%
+    dplyr::arrange(scenario, period, model) %>%
+    {.}
+  dfs = df.cmip5 %>%
+    dplyr::arrange(scenario, period, model) %>%
+    dplyr::mutate(filename = paste0(model, ".", scenario, ".csv")) %>%
+    dplyr::group_by(period, scenario) %>%
+    dplyr::group_split() %>%
+    {.}
+  data = lapply(dfs, function(df) {
+    print(paste(df$scenario[[1]], df$period[[1]]))
+    result.one.group = lapply(1:nrow(df), function(i) {
+      foldername = df$folder[[i]]
+      filename = df$filename[[i]]
+      df.data = readr::read_csv(sprintf("%s%s/%s", parent.folder, foldername, filename)) %>%
+        ## dplyr::filter(Missing == 0) %>%
+        dplyr::mutate(model = substr(proj, 1, nchar(proj) - 6)) %>%
+        dplyr::mutate(scenario = substr(proj, nchar(proj) - 4, nchar(proj))) %>%
+        dplyr::mutate(folder = foldername, file = filename) %>%
+        dplyr::select(-proj) %>%
+        {.}
+    })
+    df.one.group = dplyr::bind_rows(result.one.group) %>%
+      dplyr::mutate(period = df$period[[1]]) %>%
+      {.}
+  }) %>%
+  dplyr::bind_rows() %>%
+  {.}
+  return(data)
+}) %>%
+dplyr::bind_rows() %>%
+{.}
 
-result = lapply(folders, function(f) {
-  lines = readLines(paste0("cmip5/bcca/", f, "/MetaData.txt"))
-  period.line = lines[[5]]
-  period = gsub("Period:                ", "", period.line)
-  model.lines <- readLines(paste0("cmip5/bcca/", f, "/Projections5.txt"))
-  model = sapply(model.lines, function (m) {substr(m, 1, nchar(m) - 6)})
-  scenario = sapply(model.lines, function (m) {substr(m, nchar(m) - 4, nchar(m))})
-  return(tibble::tibble("model"=model, "period"=period, "scenario"=scenario, "folder"=f))
-})
-
-df.cmip5 = dplyr::bind_rows(result)
-
-df.cmip5 %>%
-  dplyr::arrange(scenario, period, model) %>%
-  readr::write_csv("cmip5_download.csv")
-
-dfs = df.cmip5 %>%
-  dplyr::arrange(scenario, period, model) %>%
-  dplyr::mutate(filename = paste0(model, ".", scenario, ".csv")) %>%
-  dplyr::group_by(period, scenario) %>%
-  dplyr::group_split() %>%
+compiled.data %>%
+  dplyr::left_join(building_location,
+                   by=c("lat.pts"="Latitude", "lon.pts"="Longitude360")) %>%
+  dplyr::filter(BLDGNUM == "AL0000AB", scenario == "rcp45", model == "access1-0.1", year==2014) %>%
   {.}
 
-result = lapply(dfs, function(df) {
-  print(paste(df$scenario[[1]], df$period[[1]]))
-  result.one.group = lapply(1:nrow(df), function(i) {
-    foldername = df$folder[[i]]
-    filename = df$filename[[i]]
-    df.data =
-      readr::read_csv(sprintf("cmip5/bcca/%s/%s", foldername, filename)) %>%
-      dplyr::filter(Missing == 0) %>%
-      dplyr::mutate(model = substr(proj, 1, nchar(proj) - 6)) %>%
-      dplyr::mutate(scenario = substr(proj, nchar(proj) - 4, nchar(proj))) %>%
-      dplyr::select(-proj) %>%
-      {.}
-  })
-  df.one.group = dplyr::bind_rows(result.one.group) %>%
-    dplyr::mutate(period = df$period[[1]]) %>%
-    {.}
-})
+compile.data.relabel = compiled.data %>%
+  dplyr::filter(!(year %in% c(2059, 2099))) %>%
+  dplyr::mutate(period = ifelse(stringr::str_detect(period, c("2005Jan")) |
+                                stringr::str_detect(period, "2014Feb") |
+                                stringr::str_detect(period, "2011Jan"),
+                                "2005Jan through 2019Jan", period)) %>%
+  dplyr::distinct() %>%
+  {.}
 
-dplyr::bind_rows(result) %>%
+compile.data.relabel %>%
+  dplyr::distinct(model, scenario, period) %>%
+  readr::write_csv("cmip5_download.csv")
+
+compile.data.relabel %>%
   feather::write_feather("../data/cmip5_concat.feather")
 
-summary.result = lapply(result, function(df) {
-  df.summary = df %>%
-    dplyr::group_by(scenario, period, model, lat.pts, lon.pts) %>%
-    dplyr::summarise(n()) %>%
-    dplyr::ungroup() %>%
-    {.}
-})
-
-df.summary.all = dplyr::bind_rows(summary.result)
+df.summary.all = compile.data.relabel %>%
+  dplyr::group_by(scenario, period, model, lat.pts, lon.pts) %>%
+  dplyr::summarise(count=n(), missing=sum(Missing)) %>%
+  dplyr::ungroup() %>%
+  {.}
 
 load("../data/energy_monthly_web_continental.rda")
 load("../data/retrofit.avg.energy.enough.data.rda")
+load("../data/retrofit.alldata.rda")
 load("../data/building_location.rda")
 
-retrofit.buildings = unique(retrofit.avg.energy.enough.data$BLDGNUM)
+retrofit.buildings = unique(retrofit.alldata$BLDGNUM)
 
 building_location <- building_location %>%
   dplyr::select(BLDGNUM, Latitude, Longitude) %>%
@@ -75,28 +95,20 @@ building_location %>%
   {.}
 
 df.summary.all <- df.summary.all %>%
+  dplyr::mutate(`cmip5 availability`=ifelse(missing > 0, "no data", "with data")) %>%
   dplyr::left_join(building_location,
                    by=c("lat.pts"="Latitude", "lon.pts"="Longitude360")) %>%
+  dplyr::rename(Latitude = lat.pts) %>%
+  dplyr::select(-lon.pts) %>%
   {.}
 
 df.summary.all %>%
   readr::write_csv("cmip5_summary_count.csv")
 
-no.cmip5.data = energy_monthly_web_continental %>%
-  dplyr::distinct(BLDGNUM) %>%
-  dplyr::filter(!BLDGNUM %in% unique(df.summary.all$BLDGNUM)) %>%
-  dplyr::left_join(building_location, by="BLDGNUM") %>%
-  dplyr::mutate(`cmip5 availability` = "no data") %>%
-  dplyr::select(-Longitude360) %>%
-  {.}
-
 library("sf")
 
 to.plot = df.summary.all %>%
-  distinct(BLDGNUM, lat.pts, Longitude) %>%
-  dplyr::rename(Latitude = lat.pts) %>%
-  dplyr::mutate(`cmip5 availability` = "with data") %>%
-  dplyr::bind_rows(no.cmip5.data) %>%
+  distinct(BLDGNUM, Latitude, Longitude, `cmip5 availability`) %>%
   {.}
 
 ## for retrofit data set
@@ -107,20 +119,20 @@ cnt = to.plot %>%
   dplyr::ungroup() %>%
   {.}
 
-to.plot %>%
-  dplyr::filter(BLDGNUM %in% retrofit.buildings) %>%
-  dplyr::mutate(`cmip5 availability` =
-                  ifelse(`cmip5 availability` == "with data",
-                         paste0("with data (n=", cnt$`n()`[[2]], ")"),
-                         paste0("no data (n=", cnt$`n()`[[1]], ")"))) %>%
-  ggplot2::ggplot(ggplot2::aes(x=Longitude, y=Latitude,
-                               color=`cmip5 availability`)) +
-  ggplot2::borders("state", colour="black", fill=NA) +
-  ggplot2::coord_quickmap() +
-  ggplot2::geom_point(size=0.5) +
-  ggplot2::ggtitle("retrofit data set cmip5 availability") +
-  ggplot2::theme()
-ggplot2::ggsave("../images/retrofit_cmip5_loc.png", width=5, height=3)
+## to.plot %>%
+##   dplyr::filter(BLDGNUM %in% retrofit.buildings) %>%
+##   dplyr::mutate(`cmip5 availability` =
+##                   ifelse(`cmip5 availability` == "with data",
+##                          paste0("with data (n=", cnt$`n()`[[2]], ")"),
+##                          paste0("no data (n=", cnt$`n()`[[1]], ")"))) %>%
+##   ggplot2::ggplot(ggplot2::aes(x=Longitude, y=Latitude,
+##                                color=`cmip5 availability`)) +
+##   ggplot2::borders("state", colour="black", fill=NA) +
+##   ggplot2::coord_quickmap() +
+##   ggplot2::geom_point(size=0.5) +
+##   ggplot2::ggtitle("retrofit data set cmip5 availability") +
+##   ggplot2::theme()
+## ggplot2::ggsave("../images/retrofit_cmip5_loc.png", width=5, height=3)
 
 ## for long-term data set
 load("../data/energy_90_to_18.rda")
@@ -159,17 +171,36 @@ bin.result = lapply(result, function(df) {
     {.}
 })
 
-cmip5.bin.year = dplyr::bind_rows(bin.result) %>%
+options(tibble.width=Inf)
+
+cmip5.bin.year = compile.data.relabel %>%
+  dplyr::group_by(lat.pts, lon.pts, year, model, scenario, period) %>%
+  dplyr::summarise_if(is.numeric, sum) %>%
+  dplyr::ungroup() %>%
   dplyr::left_join(building_location,
-                  by=c("lat.pts"="Latitude", "lon.pts"="Longitude360")) %>%
-  dplyr::select(-lon.pts, -Missing) %>%
+                   by=c("lat.pts"="Latitude", "lon.pts"="Longitude360")) %>%
+  dplyr::select(-lon.pts) %>%
   dplyr::rename(Latitude=lat.pts) %>%
   dplyr::select(BLDGNUM, Latitude, Longitude, period, year, scenario, model, everything()) %>%
   {.}
 
-usethis::use_data(cmip5.bin.year)
+load("../data/cmip5.bin.year.rda")
+
+## check how whether all bins summ to 365
+cmip5.bin.year %>%
+  ## dplyr::filter(Missing == 0) %>%
+  dplyr::select(-Missing) %>%
+  tidyr::gather(temperature.bin, value, `<10`:`>90`) %>%
+  dplyr::group_by(BLDGNUM, period, scenario, year, model) %>%
+  dplyr::summarise(value = sum(value)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct(value) %>%
+  {.}
+
+usethis::use_data(cmip5.bin.year, overwrite = T)
 
 cmip5.bin.period = cmip5.bin.year %>%
+  dplyr::filter(!(year %in% c(2014, 2059, 2099))) %>%
   dplyr::select(-year) %>%
   dplyr::group_by(BLDGNUM, Latitude, Longitude, period, scenario, model) %>%
   dplyr::summarise_if(is.numeric, mean) %>%
@@ -180,4 +211,18 @@ cmip5.bin.period = cmip5.bin.year %>%
   dplyr::ungroup() %>%
   {.}
 
-usethis::use_data(cmip5.bin.period)
+usethis::use_data(cmip5.bin.period, overwrite = T)
+
+cmip5.bin.period.threeyear = cmip5.bin.year %>%
+  dplyr::filter((2054 <= year & year <= 2056) | (2094 <= year & year <= 2096)) %>%
+  dplyr::select(-year) %>%
+  dplyr::group_by(BLDGNUM, Latitude, Longitude, period, scenario, model) %>%
+  dplyr::summarise_if(is.numeric, mean) %>%
+  dplyr::ungroup() %>%
+  ## filter the model who don't appear in all scenario
+  dplyr::group_by(BLDGNUM, model) %>%
+  dplyr::filter(n() == 4) %>%
+  dplyr::ungroup() %>%
+  {.}
+
+usethis::use_data(cmip5.bin.period.threeyear, overwrite = T)
