93511dc967489140afca9f051d438a14a3dce5f7
yujiex <shenqing1225@gmail.com>
2020-06-09 03:44:09 -0400

    Read data from new folder, change start time from hardcode to metadata
    Following uses the metadata info to determine start time of the model
    as.Date(sprintf("%s-01", substr(period, 1, 7)), format='%Y%b-%d')

--- data-raw/read_bcca.R
+++ data-raw/read_bcca.R
@@ -9,6 +9,27 @@ load("../data/building_location.rda")
 load("../data/energy_monthly_web_continental.rda")
 load("../data/retrofit.avg.energy.enough.data.rda")
 
+folders = list.files("cmip5/bcca_now/", "bcca5*")
+
+## ## check downloads
+## result = lapply(folders, function(f) {
+##   lines = readLines(paste0("cmip5/bcca_now/", f, "/MetaData.txt"))
+##   period.line = lines[[5]]
+##   period = gsub("Period:                ", "", period.line)
+##   model.lines <- readLines(paste0("cmip5/bcca_now/", f, "/Projections5.txt"))
+##   model = sapply(model.lines, function (m) {substr(m, 1, nchar(m) - 6)})
+##   scenario = sapply(model.lines, function (m) {substr(m, nchar(m) - 4, nchar(m))})
+##   return(tibble::tibble("model"=model, "period"=period, "scenario"=scenario, "folder"=f))
+## })
+
+## result %>%
+##   dplyr::bind_rows() %>%
+##   dplyr::distinct(model, period, scenario) %>%
+##   dplyr::group_by(period, scenario) %>%
+##   dplyr::count() %>%
+##   dplyr::ungroup() %>%
+##   {.}
+
 exist.location = readr::read_csv("cmip5/bcca/bcca5/canesm2.1.rcp45.csv") %>%
   dplyr::distinct(lat.pts, lon.pts) %>%
   dplyr::mutate(has.data=T) %>%
@@ -21,9 +42,9 @@ uniqueLocation <- building_location %>%
   dplyr::rename(latitude=Latitude, longitude=Longitude) %>%
   distinct(latitude, longitude) %>%
   dplyr::mutate(longitude360=longitude %% 360) %>%
-  ## only process locations not already with data
-  dplyr::left_join(exist.location, by=c("latitude"="lat.pts", "longitude360"="lon.pts")) %>%
-  dplyr::filter(is.na(has.data)) %>%
+  ## ## only process locations not already with data
+  ## dplyr::left_join(exist.location, by=c("latitude"="lat.pts", "longitude360"="lon.pts")) %>%
+  ## dplyr::filter(is.na(has.data)) %>%
   {.}
 
 ## locations to get weather for
@@ -31,37 +52,42 @@ lon.pts <- uniqueLocation$longitude360
 lat.pts <- uniqueLocation$latitude
 extract.pts <- cbind(lon.pts,lat.pts)
 
-folders = list.files("cmip5/bcca/", "bcca5*")
+folders = list.files("cmip5/bcca_now/", "bcca5*")
+## folders = list.files("cmip5/bcca/", "bcca5*")
 
-for (f in folders) {
-  lines = readLines(paste0("cmip5/bcca/", f, "/MetaData.txt"))
+for (f in folders[10:34]) {
+  ## lines = readLines(paste0("cmip5/bcca/", f, "/MetaData.txt"))
+  lines = readLines(paste0("cmip5/bcca_now/", f, "/MetaData.txt"))
   period.line = lines[[5]]
   period = gsub("Period:                ", "", period.line)
-  startdate = as.Date(sprintf("%s-01-01", substr(period, 1, 4)))
-
-  ncdfData = ncdf4::nc_open(paste0("cmip5/bcca/", f, "/Extraction_tasmax.nc"))
-
+  startdate = as.Date(sprintf("%s-01", substr(period, 1, 7)), format='%Y%b-%d')
+  if (!stringr::str_detect(period, "2014Feb")) {
+    print(startdate)
+    next
+  }
+  print(startdate)
+  ## ncdfData = ncdf4::nc_open(paste0("cmip5/bcca/", f, "/Extraction_tasmax.nc"))
+  ncdfData = ncdf4::nc_open(paste0("cmip5/bcca_now/", f, "/Extraction_tasmax.nc"))
   proj = ncdf4::ncatt_get(ncdfData, varid=0, attname="Projections")
   projections = unlist(strsplit(proj$value, split=", "))
   nsteps = dim(ncdf4::ncvar_get(ncdfData, varid = "time"))
   print(projections)
   ncdf4::nc_close(ncdfData)
-
   break_labels = c("<10", sprintf("[%s-%s)", seq(10, 80, by=10), seq(20, 90, by=10)), ">90", "Missing")
-
   for (i in 1:length(projections)) {
     proj = projections[[i]]
-    tasmax = raster::brick(sprintf("cmip5/bcca/%s/Extraction_tasmax.nc", f), lvar=4, level=i)
-    tasmin = raster::brick(sprintf("cmip5/bcca/%s/Extraction_tasmin.nc", f), lvar=4, level=i)
+    ## tasmax = raster::brick(sprintf("cmip5/bcca/%s/Extraction_tasmax.nc", f), lvar=4, level=i)
+    ## tasmin = raster::brick(sprintf("cmip5/bcca/%s/Extraction_tasmin.nc", f), lvar=4, level=i)
+    tasmax = raster::brick(sprintf("cmip5/bcca_now/%s/Extraction_tasmax.nc", f), lvar=4, level=i)
+    tasmin = raster::brick(sprintf("cmip5/bcca_now/%s/Extraction_tasmin.nc", f), lvar=4, level=i)
     tasavg = (tasmax + tasmin) / 2
-    acc = NULL
     ## spatial interpolation at each step
     result = lapply(1:nsteps, function(j) {
       if (j %% 100 == 0) {
         print(j)
       }
       ext <- raster::extract(tasavg[[j]], extract.pts, method="bilinear")
-      df <- data.frame(lat.pts, lon.pts, ext, step=j, proj) %>%
+      df <- data.frame(lat.pts, lon.pts, ext, step=j, proj=proj) %>%
         {.}
       return(df)
     })
@@ -90,8 +116,10 @@ for (f in folders) {
       tidyr::spread(key=`value_label`, value=`value_count`, fill=0) %>%
       dplyr::mutate_at(vars(ends_with(")"), ends_with("0")), funs(ifelse(is.na(.), 0L, .))) %>%
       {.}
-    output.filename = sprintf("cmip5/bcca/%s/%s.csv", f, proj)
-    if (file.exists(output.filename)) {
+    ## output.filename = sprintf("cmip5/bcca/%s/%s.csv", f, proj)
+    output.filename = sprintf("cmip5/bcca_now/%s/%s.csv", f, proj)
+    if (FALSE) {
+      ## if (file.exists(output.filename)) {
       readr::read_csv(output.filename) %>%
         dplyr::bind_rows(df.output) %>%
         readr::write_csv(output.filename)
