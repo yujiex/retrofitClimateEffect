e02981a0143cbc45a186f3f13f9684e67eaf0cc5
yujiex <shenqing1225@gmail.com>
2020-06-23 23:27:09 -0400

    Added cost estimation of capital and operational actions

--- data-raw/cost_effective.R
+++ data-raw/cost_effective.R
@@ -28,33 +28,63 @@ features = retrofit.alldata %>%
   dplyr::select(-`Building_Type`) %>%
   {.}
 
-## fit highlevel
-dfs = retrofit_from_db %>%
-  dplyr::distinct(Building_Number, Substantial_Completion_Date, high_level_ECM, source_highlevel) %>%
-  dplyr::mutate(value = 1) %>%
-  tidyr::spread(high_level_ECM, value, fill=0) %>%
-  dplyr::left_join(cost.lightouch, by=c("Building_Number", "Substantial_Completion_Date")) %>%
-  dplyr::left_join(features, by=c("Building_Number"="BLDGNUM")) %>%
-  na.omit() %>%
-  dplyr::mutate(cost.per.sqft = cost/GROSSSQFT) %>%
-  dplyr::select(-`Building_Number`, -`Substantial_Completion_Date`,
-                -`source_highlevel`, -cost) %>%
-  ## dplyr::group_by(historic, is.office) %>%
-  dplyr::group_by(historic) %>%
-  dplyr::group_split() %>%
-  {.}
+kw = "toplevel"
+kw = "highlevel"
 
-result = lapply(dfs, function(df) {
-  historic = df$historic[[1]]
-  ## office = df$is.office[[1]]
-  cost.est = lm(cost.per.sqft~`Advanced Metering` + `Building Envelope` + `Building Tuneup or Utility Improvements` + `HVAC` + `Lighting`, data=df)
-  df.result = data.frame(summary(cost.est)$coefficients) %>%
-    tibble::rownames_to_column("action") %>%
-    dplyr::mutate(historic = historic) %>%
-    ## dplyr::mutate(historic = historic, is.office = office) %>%
-    tibble::as_tibble() %>%
+## fit toplevel
+if (kw == "toplevel") {
+  dfs = retrofit_from_db %>%
+    dplyr::distinct(Building_Number, Substantial_Completion_Date, high_level_ECM) %>%
+    dplyr::mutate(top_level_ECM = ifelse(high_level_ECM %in% c("Advanced Metering", "GSALink", "Building Tuneup or Utility Improvements"), "Operational", "Capital")) %>%
+    dplyr::distinct(`Building_Number`, Substantial_Completion_Date, top_level_ECM) %>%
+    dplyr::arrange(`Building_Number`, Substantial_Completion_Date, top_level_ECM) %>%
+    dplyr::group_by(`Building_Number`, Substantial_Completion_Date) %>%
+    dplyr::summarise(top_level_ECM = paste(top_level_ECM, collapse = " and ")) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(value = 1) %>%
+    tidyr::spread(top_level_ECM, value, fill=0) %>%
+    dplyr::inner_join(cost.lightouch, by=c("Building_Number", "Substantial_Completion_Date")) %>%
+    dplyr::inner_join(features, by=c("Building_Number"="BLDGNUM")) %>%
+    dplyr::mutate(cost.per.sqft = cost/GROSSSQFT) %>%
+    dplyr::select(-`Building_Number`, -`Substantial_Completion_Date`, -cost) %>%
+    dplyr::group_by(historic) %>%
+    dplyr::group_split() %>%
     {.}
-})
+  result = lapply(dfs, function(df) {
+    historic = df$historic[[1]]
+    cost.est = lm(cost.per.sqft~Capital + Operational + `Capital and Operational`-1, data=df)
+    df.result = data.frame(summary(cost.est)$coefficients) %>%
+      tibble::rownames_to_column("action") %>%
+      dplyr::mutate(historic = historic) %>%
+      tibble::as_tibble() %>%
+      {.}
+  })
+} else if (kw == "highlevel") {
+  dfs = retrofit_from_db %>%
+    dplyr::distinct(Building_Number, Substantial_Completion_Date, high_level_ECM, source_highlevel) %>%
+    dplyr::filter(high_level_ECM != "GSALink") %>%
+    dplyr::mutate(value = 1) %>%
+    tidyr::spread(high_level_ECM, value, fill=0) %>%
+    dplyr::left_join(cost.lightouch, by=c("Building_Number", "Substantial_Completion_Date")) %>%
+    dplyr::left_join(features, by=c("Building_Number"="BLDGNUM")) %>%
+    na.omit() %>%
+    dplyr::mutate(cost.per.sqft = cost/GROSSSQFT) %>%
+    dplyr::select(-`Building_Number`, -`Substantial_Completion_Date`,
+                  -`source_highlevel`, -cost) %>%
+    ## dplyr::group_by(historic, is.office) %>%
+    dplyr::group_by(historic) %>%
+    dplyr::group_split() %>%
+    {.}
+  result = lapply(dfs, function(df) {
+    historic = df$historic[[1]]
+    cost.est = lm(cost.per.sqft~`Advanced Metering` + `Building Envelope` + `Building Tuneup or Utility Improvements` + `HVAC` + `Lighting`, data=df)
+    df.result = data.frame(summary(cost.est)$coefficients) %>%
+      tibble::rownames_to_column("action") %>%
+      dplyr::mutate(historic = historic) %>%
+      tibble::as_tibble() %>%
+      {.}
+  })
+}
 
 cost.est.result = result %>%
   dplyr::bind_rows() %>%
@@ -62,13 +92,11 @@ cost.est.result = result %>%
   {.}
 
 cost.est.result %>%
-  readr::write_csv("action_cost_with_ci.csv")
-
+  readr::write_csv(sprintf("%s_action_cost_with_ci.csv", kw))
 cost.est.result %>%
   dplyr::select(action, Estimate, historic) %>%
   dplyr::filter(action != "(Intercept)") %>%
-  readr::write_csv("action_cost.csv")
-
+  readr::write_csv(sprintf("%s_action_cost.csv", kw))
 est = cost.est.result %>%
   dplyr::select(action, Estimate, historic) %>%
   dplyr::filter(action != "(Intercept)") %>%
@@ -81,12 +109,11 @@ err = cost.est.result %>%
   tidyr::spread(action, `Std..Error`) %>%
   dplyr::mutate(field = "stderr") %>%
   {.}
-
 est %>%
   dplyr::bind_rows(err) %>%
   dplyr::arrange(historic, field) %>%
   dplyr::select(field, everything()) %>%
-  readr::write_csv("action_cost_wide.csv")
+  readr::write_csv(sprintf("%s_action_cost_wide.csv", kw))
 
 ## fit detaillevel
 dfs.detail = retrofit_from_db %>%
@@ -107,12 +134,16 @@ dfs.detail = retrofit_from_db %>%
 df = dfs.detail[[1]]
 historic = df$historic[[1]]
 df.model = df %>%
-  dplyr::select(-is.office, -historic, -GROSSSQFT) %>%
+  dplyr::select(-is.office, -historic, -GROSSSQFT, -GSALink) %>%
   {.}
+
 cost.est = lm(cost.per.sqft~., data=df.model)
+summary(cost.est)
 
 ## fit with non-negative least square, there are many 0 coefs
 mat.model = df.model %>%
   dplyr::select(-cost.per.sqft) %>%
   as.matrix()
 constraint.reg.result = nnls::nnls(A=mat.model, b=df.model$cost.per.sqft)
+tibble::tibble(variable = colnames(mat.model), coef=constraint.reg.result$x) %>%
+  print(n=Inf)
